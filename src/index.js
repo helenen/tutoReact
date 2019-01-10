c123d34f6aa799c566d813074774959c034349a4
helenen <helene.niassy@gmail.com>
2019-01-10 17:05:34 +0100

    move up in tuto

--- src/index.js
+++ src/index.js
@@ -4,17 +4,34 @@ import "./index.css";
 
 class Square extends React.Component {
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button
+        className="square"
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
+    );
   }
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
-  }
-
-  render() {
-    const status = "Next player: X";
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      };
+    }
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
+    renderSquare(i) {
+      return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>;
+    }
+    render() {
+      const status = 'Next player: X';
 
     return (
       <div>
@@ -57,4 +74,7 @@ class Game extends React.Component {
 
 // ========================================
 
-ReactDOM.render(<Game />, document.getElementById("root"));
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
